da66c8301c1a958f5a6f09ae4194f86e6298084d
gerson99 <43899551+gerson99@users.noreply.github.com>
2020-12-11 15:11:38 -0600

    Create pagina2.php

---
+++ pagina2.php
@@ -0,0 +1,8 @@
+<?php
+$nombre = $_POST["nombre"]; 
+$apellido = $_POST["apellido"];
+$email = $_POST["email"];
+echo "Hola ".$nombre." ".
+$apellido.", <br />
+Te has registrado con el siguiente correo electronico: " . $email . ".";
+?>
