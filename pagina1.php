2b05eef01ffaf90a2d05a057c081e27918ac6c11
gerson99 <43899551+gerson99@users.noreply.github.com>
2020-12-11 14:52:26 -0600

    Update pagina1.php

--- pagina1.php
+++ pagina1.php
@@ -3,7 +3,7 @@
 <form method="post" action="pagina2.php" >
 Por favor, envia la newsletter a: <br />
 Tu nombre: <input type="text" name="nombre" /><br />
-Tu apellido: <input type="text" name="email" /><br />
+Tu apellido: <input type="text" name="apellido" /><br />
 Tu correo electronico: <input type="text" name="email" /><br />
 <input type="submit" value="Enviar formulario" />
 </form>
