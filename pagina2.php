4a90a4d15542e75fd9e975e65bae7568489f8956
gerson99 <43899551+gerson99@users.noreply.github.com>
2020-12-11 14:51:28 -0600

    Update pagina2.php

--- pagina2.php
+++ pagina2.php
@@ -3,7 +3,7 @@
 $nombre = $_POST["nombre"];
 $apellido = $_POST["apellido"];
 $email = $_POST["email"];
-echo "Hola " . $nombre . " ".
-$apellido . ", <br />
-Te has registrado con el siguiente correo electronico: " . $email .".";
+echo "Hola " .$nombre. "".
+$apellido. ", <br />
+Te has registrado con el siguiente correo electronico:".$email.".";
 ?>
